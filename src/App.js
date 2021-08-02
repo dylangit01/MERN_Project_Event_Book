462e5a4b7e0a6415c3081ff31fd9884775e2cbbc
Dylan <dcren2010@gmail.com>
2021-08-02 10:45:40 -0400

    remove unused file

--- src/App.js
+++ src/App.js
@@ -5,33 +5,31 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import Footer from './components/Footer/Footer'
-import About from './components/About/About'
+import Footer from './components/Footer/Footer';
+import About from './components/About/About';
 
 const theme = createMuiTheme({
-  typography: {
-    fontFamily: [
-      'Ubuntu', 'sans-serif',
-    ]
-  }
+	typography: {
+		fontFamily: ['Ubuntu', 'sans-serif'],
+	},
 });
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <ThemeProvider theme = {theme}>
-        <Container maxWidth='lg'>
-          <Navbar />
-          <Switch>
-            <Route path='/' exact component={ Home }/>
-            <Route path='/auth' exact component={ Auth }/>
-          </Switch>
-        </Container>
-        <Route path='/about' component={About} />
-         <Footer />
-      </ThemeProvider>
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<ThemeProvider theme={theme}>
+				<Container maxWidth='lg'>
+					<Navbar />
+					<Switch>
+						<Route path='/' exact component={Home} />
+						<Route path='/auth' exact component={Auth} />
+					</Switch>
+				</Container>
+				<Route path='/about' component={About} />
+				<Footer />
+			</ThemeProvider>
+		</BrowserRouter>
+	);
 };
 
 export default App;
